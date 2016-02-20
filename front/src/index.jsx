afb413f0cc108b181d26d5bb33568f171c630c54
Айдар Ибатуллин <amazing.space.invader@gmail.com>
2016-02-20 19:21:02 +0300

    test

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom'
 
 import Wrapper from './components/wrapper/Wrapper.jsx'
 
+console.log('delete me');
+
 ReactDOM.render(
     <Wrapper />,
     document.getElementById('app')
