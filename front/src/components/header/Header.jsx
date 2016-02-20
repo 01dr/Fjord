2243cb2d581522ebb8ff906907ea32c3c2ec4d2b
Айдар Ибатуллин < >
2016-02-20 12:45:11 +0300

    add header

---
+++ front/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by amazi on 20.02.2016.
+ */
+
+import React from 'react'
+import classNames from 'classnames/bind'
+import s from './header.pcss'
+
+class Header extends React.Component {
+    render() {
+        return  <div className={ s.background }></div>;
+    }
+}
+
+export default Header
--- front/src/components/wrapper/wrapper.jsx
+++ front/src/components/wrapper/wrapper.jsx
@@ -7,10 +7,15 @@ import classNames from 'classnames/bind'
 import s from './wrapper.pcss'
 
 import Menu from '../menu/Menu.jsx'
+import Header from '../header/Header.jsx'
 
 class Wrapper extends React.Component {
     render() {
-        return  <div>
+        return  <div className={ s.wrapper }>
+                    <Header />
+                    <div className={ s.container }>
+                        in container
+                    </div>
                     I'm Wrapper!
                     <Menu />
                 </div>
